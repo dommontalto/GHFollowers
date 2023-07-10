fc21826bb7bf3b043f61c5462ebeeec5548cf8bc
Dom Montalto <dommontalto@outlook.com>
2023-07-10 20:47:40 +0100

    Network refactor

--- GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/Controllers/FollowerListVC.swift
@@ -10,21 +10,20 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-    
-    
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers else {
-                self.presentGFAlertOnMainThread(alertTitle: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
-            }
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
-            print("Followers.count \(followers.count)")
-            print(followers)
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(alertTitle: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+            }
         }
     }
     
